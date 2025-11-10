76bff14c29a7330e89fc820de161735155b97b2d
Kerkerry <kerrywangamati24@gmail.com>
2025-11-10 16:27:00 +0300

    feat: define post collection schema and add blog page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,14 @@
-import {z, defineCollection} from "astro:content"
-import {format} from "date-fns"
+import { defineCollection,z } from "astro:content";
 
-const authorsCollection = defineCollection({
-    schema: ({image}) =>
-        z.object({
-            name: z.string(),
-            image: image(),
-        }),
+const postCollection=defineCollection({
+    schema: z.object({
+        author: z.string(),
+        date: z.string(),
+        image:z.string(),
+        title:z.string()
+    })
 })
 
-const postsCollection = defineCollection({
-    schema: ({image}) =>
-        z.object({
-            author: z.string(),
-            categories: z.array(z.string()),
-            date: z
-                .string()
-                .transform(str => format(new Date(str), "MMMM d, yyyy")),
-            featured: z.boolean().default(false),
-            image: image(),
-            title: z.string(),
-        }),
-})
-
-export const collections = {
-    authors: authorsCollection,
-    posts: postsCollection,
+export const collections={
+    posts:postCollection
 }
